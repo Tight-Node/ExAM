35fb3c89919bbe5f856a654c4dcb13d43df2ec8f
Thiago Mallon <thiagomallon@gmail.com>
2016-10-24 09:45:36 -0200

    Deleting jpg and png font images

--- routes/armory.js
+++ routes/armory.js
@@ -1,31 +1,31 @@
 var express = require('express'),
-  mongoose = require('mongoose'),
-  router = express.Router({
-    caseSensitive: false,
-    strict: true
-  });
+    mongoose = require('mongoose'),
+    router = express.Router({
+        caseSensitive: false,
+        strict: true
+    });
 
 var GunDAO = require('../models/DAO/gunsDAO'),
-  gunDAO = new GunDAO();
+    gunDAO = new GunDAO();
 
 
 router.get('/:category?/:brand?/:caliber?/:serie?', function(req, res) {
-  gunDAO.read(req.params, function(err, guns) {
-    res.render('armory/list', {
-      guns: guns,
-      title: 'Guns',
-    });
-  })
+    gunDAO.read(req.params, function(err, guns) {
+        res.render('armory/list', {
+            guns: guns,
+            title: 'Guns',
+        });
+    })
 });
 
 router.get('/', function(req, res) {
-  console.log('we\'re on optional / router');
-  gunDAO.read({}, function(err, guns) {
-    res.render('armory/list', {
-      guns: guns,
-      title: 'Guns',
-    });
-  })
+    console.log('we\'re on optional / router');
+    gunDAO.read({}, function(err, guns) {
+        res.render('armory/list', {
+            guns: guns,
+            title: 'Guns',
+        });
+    })
 });
 
 module.exports = router;
