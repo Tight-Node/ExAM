042ed44a129791a73161aa377550facfa3a92405
Thiago Mallon <thiagomallon@gmail.com>
2016-10-23 22:20:39 -0200

    Updating persistence layer structure

--- models/DAO/gunsDAO.js
+++ models/DAO/gunsDAO.js
@@ -4,8 +4,27 @@ function GunDAO() {
 
 }
 
-GunDAO.prototype.readAll = function(cb) {
-  return Gun.find().exec(cb);
+GunDAO.prototype.read = function(params, cb) {
+  console.log('We\'re in gunDAOf',params);
+  if (params.length) {
+    var query = {};
+    for (p in params) {
+      //     // console.log("'" + p + "'");
+      //     // console.log("'" + params[p] + "'");
+      //     // gunning.where("'" + p + "'").equals("Colt");
+      //     // gunning.where('brand').equals("Colt");
+      query.push({
+        p: {
+          // $regex: /params[p]/i
+          params
+        }
+      });
+    }
+    var gunning = Gun.find(query);
+    return gunning.exec(cb);
+  }
+  // console.log(query.length);
+  return Gun.find({}).exec(cb);
 };
 
 GunDAO.prototype.readOne = function(cb) {
@@ -13,10 +32,45 @@ GunDAO.prototype.readOne = function(cb) {
 };
 
 GunDAO.prototype.readPistols = function(params, cb) {
-  return Gun.find({
-    'category': 'pistol'
-  }).exec(cb);
+  var gunning = null;
+  if (params.length) {
+    gunning = Gun.find({
+      'category': 'pistol'
+    });
+    for (p in params) {
+      // console.log("'" + p + "'");
+      // console.log("'" + params[p] + "'");
+      // gunning.where("'" + p + "'").equals("Colt");
+      gunning.where('brand').equals("Colt");
+    }
+    gunning.exec(cb);
+  }
+  return gunning;
 };
+
+// GunDAO.prototype.readPistols = function(params, cb) {
+//   var gunning = null;
+//   if (params.brand.length) {
+//     gunning = Gun.find({
+//       'category': 'pistol'
+//     });
+//     for (p in params) {
+//       console.log("'" + p + "'");
+//       console.log("'" + params[p] + "'");
+//       // gunning.where("'" + p + "'").equals("'" + params[p] + "'");
+//       gunning.where("brand").equals("Colt");
+//     }
+//     gunning.exec(cb);
+//   }
+//   return gunning;
+// };
+
+// GunDAO.prototype.readPistols = function(params, cb) {
+//
+// return Gun.find({
+//   'category': 'pistol'
+// }).exec(cb);
+// };
 
 GunDAO.prototype.readSMGs = function(cb) {
   return Gun.find({
--- routes/armory.js
+++ routes/armory.js
@@ -8,8 +8,11 @@ var express = require('express'),
 var GunDAO = require('../models/DAO/gunsDAO'),
   gunDAO = new GunDAO();
 
-router.get('/', function(req, res) {
-  gunDAO.readAll(function(err, guns) {
+
+router.get('/:category?/:brand?/:caliber?/:serie?', function(req, res) {
+  // console.log('category:' + req.params.category);
+  console.log('we\'re on optional params router');
+  gunDAO.read(req.params, function(err, guns) {
     res.render('armory/list', {
       guns: guns,
       title: 'Guns',
@@ -17,33 +20,54 @@ router.get('/', function(req, res) {
   })
 });
 
-router.get('/pistols/:brand?', function(req, res) {
-  // console.log(req.params.brand);
-  gunDAO.readPistols(req.params,function(err, guns) {
-    console.log(guns);
+router.get('/', function(req, res) {
+  console.log('we\'re on optional / router');
+  gunDAO.read(null, function(err, guns) {
     res.render('armory/list', {
       guns: guns,
-      title: 'Pistols',
+      title: 'Guns',
     });
   })
 });
 
-router.get('/smgs', function(req, res) {
-  gunDAO.readSMGs(function(err, guns) {
-    res.render('armory/list', {
-      guns: guns,
-      title: 'SMG\'s',
-    });
-  })
-});
+// router.get('/pistols/:brand?', function(req, res) {
+//   // console.log(req.params.brand);
+//   gunDAO.readPistols(req.params, function(err, guns) {
+//     // console.log(guns);
+//     res.render('armory/list', {
+//       guns: guns,
+//       title: 'Pistols',
+//     });
+//   })
+// });
+//
+// router.get('/smgs', function(req, res) {
+//   gunDAO.readSMGs(function(err, guns) {
+//     res.render('armory/list', {
+//       guns: guns,
+//       title: 'SMG\'s',
+//     });
+//   })
+// });
+//
+// router.get('/shotguns', function(req, res) {
+//   gunDAO.readShotguns(function(err, guns) {
+//     res.render('armory/list', {
+//       guns: guns,
+//       title: 'Shotguns',
+//     });
+//   })
+// });
 
-router.get('/shotguns', function(req, res) {
-  gunDAO.readShotguns(function(err, guns) {
-    res.render('armory/list', {
-      guns: guns,
-      title: 'Shotguns',
-    });
-  })
-});
+// router.get('/:category?/:brand?/:caliber?/:serie?', function(req, res) {
+//   console.log(req.params.brand);
+//   gunDAO.read(req.params, function(err, guns) {
+//     // console.log(guns);
+//     res.render('armory/list', {
+//       guns: guns,
+//       title: 'Fine search',
+//     });
+//   })
+// });
 
 module.exports = router;
