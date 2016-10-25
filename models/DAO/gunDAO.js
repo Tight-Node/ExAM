b193a49e74c2054afa75341ec9be25eb7243cfd9
Thiago Mallon <thiagomallon@gmail.com>
2016-10-25 10:32:30 -0200

    Updating persistence layer

--- models/DAO/gunDAO.js
+++ models/DAO/gunDAO.js
@@ -5,19 +5,20 @@ function GunDAO() {
 }
 
 GunDAO.prototype.read = function(paramVal, cb) {
-    if (Object.keys(paramVal).length) {
-        var query = {};
-        for (p in paramVal) {
-            if (paramVal[p] != undefined) query[p] = paramVal[p];
-        }
-        var gunner = Gun.find(query).exec(cb);
-        return gunner;
-    }
-    return Gun.find({}).exec(cb);
+    // if (Object.keys(paramVal).length) {
+    //     var query = {};
+    //     for (p in paramVal) {
+    //         if (paramVal[p] != undefined) query[p] = paramVal[p];
+    //     }
+    //     var gunner = Gun.find(query).exec(cb);
+    //     return gunner;
+    // }
+    // return Gun.find({}).exec(cb);
+    return {};
 };
 
 GunDAO.prototype.readOne = function(cb) {
-    return Gun.findOne().exec(cb);
+    // return Gun.findOne().exec(cb);
 };
 
 module.exports = GunDAO;
--- models/gun.js
+++ models/gun.js
@@ -1,44 +1,79 @@
 /**
- * @file Gun model
+ * @file Gunes model
  */
 
 /**
+ * Represents a gun.
  * @author Thiago Mallon <thiagomallon@gmail.com>
- * @class Gun
  */
-var Gun = {};
+class Gunes {
+    /**
+     * Initializes the object
+     * @param {String} _id Sets the _id property value
+     * @param {String} brand Sets the brand property value
+     * @param {String} serie Sets the serie property value
+     * @param {String} caliber Sets the caliber property value
+     * @param {String} ammos Sets the ammos property value
+     */
+    constructor(_id, brand, serie, caliber, ammos) {
+        /**
+         * Property stores the object _id
+         * @prop {String} _id The default values for parties.
+         */
+        this._id = _id;
+        /**
+         * Property stores the object brand
+         * @prop {String} brand The default values for parties.
+         */
+        this.brand = brand;
+        /**
+         * Property stores the object brand
+         * @prop {String} serie The default values for parties.
+         */
+        this.serie = serie;
+        /**
+         * Property stores the object serie
+         * @prop {String} caliber The default values for parties.
+         */
+        this.caliber = caliber;
+        /**
+         * Property stores the object ammos
+         * @prop {String} ammos The default values for parties.
+         */
+        this.ammos = ammos;
+    }
 
-Object.defineProperty(Gun, '__id', {
-    value: 'this is gun std class',
-    writable: true,
-    enumerable: false,
-    configurable: false
-});
+    /**
+     * Returns the object _id property
+     * @returns {String} _id Returns the id from the object
+     */
+    getId() {
+        return this._id;
+    }
 
-Object.defineProperty(Gun, 'brand', {
-    get: function() {
-        console.log('getting brand property');
-        return this._brand;
-    },
-    set: function(value) {
-        console.log('setting brand property');
-        this._brand = value;
-    },
-    enumerable: true,
-    configurable: false
-});
+    /**
+     * Sets the object _id property value
+     * @param {String} value Value to set _id property
+     */
+    setId(value) {
+        this._id = value;
+    }
 
-Object.defineProperty(Gun, '_id', {
-    get: function() {
-        console.log('getting _id property');
-        return this.__id;
-    },
-    set: function(value) {
-        console.log('setting _id property');
-        this.__id = value;
-    },
-    enumerable: true,
-    configurable: false
-});
+    /**
+     * Returns the object brand property
+     * @returns {String} brand Returns the id from the object
+     */
+    getBrand() {
+        return this.brand;
+    }
 
-module.exports = Gun;
+    /**
+     * Sets the object brand property
+     * @param {String} value Value to set brand property
+     */
+    setBrand(value) {
+        this.brand = value;
+    }
+}
+
+module.exports = Gunes;
