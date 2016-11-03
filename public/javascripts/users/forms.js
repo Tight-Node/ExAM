106533b272b4aef17b3160168e0b3fdbdd331491
Thiago Mallon <thiagomallon@gmail.com>
2016-11-03 18:11:34 -0200

    Uploading view, stylesheet and js files

--- public/javascripts/users/forms.js
+++ public/javascripts/users/forms.js
@@ -1,36 +1,34 @@
 $(document).ready(function() {
-    // var dialog = null;
-    // dialog = $("#dialog-form").dialog({
-    //     autoOpen: false,
-    //     height: 400,
-    //     width: 350,
-    //     modal: true,
-    //     buttons: {
-    //         "Create an account": addUser,
-    //         Cancel: function() {
-    //             dialog.dialog("close");
-    //         }
-    //     },
-    //     close: function() {
-    //         form[0].reset();
-    //         allFields.removeClass("ui-state-error");
-    //     }
-    // });
-    // $("#create-user").button().on("click", function() {
-    //     console.log('Creating as soon as possible');
-    //     // dialog.load('http://localhost:1234/users/modify-form/').dialog("open");
-    // });
-    function getFormContent() {
-        console.log('what?');
-        // $.ajax({
-        //
-        // });
-    }
-    //
-    $("#create-user").click(function() {
-        getFormContent();
-        console.log('Creating as soon as possible');
-        // dialog.load('http://localhost:1234/users/modify-form/').dialog("open");
+    var dialog = null;
+    dialog = $("#dialog-form").dialog({
+        autoOpen: false,
+        height: 400,
+        width: 350,
+        modal: true,
+        buttons: {
+            Cancel: function() {
+                dialog.dialog("close");
+            }
+        },
+        close: function() {
+            form[0].reset();
+            allFields.removeClass("ui-state-error");
+        }
     });
 
+    /**
+     *
+     */
+    function getUserFormContent() {
+        $.ajax({
+            url: 'http://localhost:1234/users/modify-form/5818c2e3804dc77d2a5602dd'
+        }).done(function(res) {
+            $("#dialog-form").html('what?');
+            dialog.dialog("open");
+        });
+    }
+
+    $("#edit-user").button().on("click", function() {
+        getUserFormContent();
+    });
 });
