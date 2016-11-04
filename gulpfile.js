ae9f3f047a67e711e014771d8ff295abaebab1ce
Thiago Mallon <thiagomallon@gmail.com>
2016-11-04 19:04:59 -0200

    Updating gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,38 +2,50 @@
     'use strict';
 
     var gulp = require('gulp'),
+        clean = require('gulp-clean'),
         concat = require('gulp-concat'),
         jshint = require('gulp-jshint'),
+        jsdoc = require('gulp-jsdoc3'),
         uglify = require('gulp-uglify'),
         sass = require('gulp-sass');
+
+    gulp.task('clean', function() {
+        return gulp.src('./docs', {
+            read: false
+        }).pipe(clean({
+            force: true
+        }));
+    });
 
     // --- Basic Tasks ---
     gulp.task('css', function() {
         return gulp.src('./public/stylesheets/sass/*.scss')
-            .pipe(
-                sass({
-                    includePaths: ['./public/stylesheets/'],
-                    errLogToConsole: true,
-                    outputStyle: 'compressed'
-                }))
+            .pipe(sass({
+                includePaths: ['./public/stylesheets/'],
+                errLogToConsole: true,
+                outputStyle: 'compressed'
+            }))
             .pipe(concat('style.css'))
             .pipe(gulp.dest('./public/stylesheets/'));
     });
 
-    gulp.task('js', function() {
-        return gulp.src('./src/*.js')
-            .pipe(concat('all.min.js'))
-            .pipe(uglify({
-                mangle: false
-            }))
-            .pipe(gulp.dest('./public/javascripts/dist/'));
+    gulp.task('hint', function() {
+        return gulp.src(['./libs/*.js', './models/*.js', './models/DAO/*.js', './routes/*.js'])
+            .pipe(jshint())
+            .pipe(jshint.reporter('jshint-stylish'))
+            .pipe(jshint.reporter('fail'));
+    });
+
+    gulp.task('doc', function(cb) {
+        let config = require('./jsdoc-conf');
+        gulp.src(['./libs/*.js', './models/*.js', './models/DAO/*.js', './routes/*.js'], {
+                read: false
+            })
+            .pipe(jsdoc(config, cb));
     });
 
     gulp.task('watch', function() {
-        gulp.watch('./public/stylesheets/sass/*.scss', ['css']);
-        // gulp.watch('./src/*.js', ['js']);
+        gulp.watch('./public/stylesheets/*.scss', ['css']);
+        gulp.watch(['./libs/*.js', './models/*.js', './models/DAO/*.js', './routes/*.js'], ['hint']);
     });
-
-    // gulp.task('default', ['css', 'js', 'watch']);
-    // gulp.task('build', ['css', 'js']);
 }());
